896ffa796e94eedb8be0cf75b53b3e8378ea681d
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-24 23:21:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
 }
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, obj)
-  newObj
+  var newObj = Object.assign({}, object)
   delete newObj[key]
   return newObj
+  
 }
