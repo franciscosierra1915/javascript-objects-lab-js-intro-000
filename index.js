1f33ea61c58326f3a20eba1532f6a340b41ac430
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-24 23:15:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {egg: 1}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+  
+}
